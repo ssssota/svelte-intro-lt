4062150ea1fd0332ca3779e2af47644b8faec20c
Sotaro Tommykawa <tommysouta@gmail.com>
2021-01-13 21:50:55 +0900

    ✨ Add qr code for github

--- src/pages/Page17.svelte
+++ src/pages/Page17.svelte
@@ -3,12 +3,17 @@ import CenteringPane from "../components/CenteringPane.svelte";
 </script>
 
 <CenteringPane>
-  <h1>実はこのスライドも</h1>
-  <h1>Svelte製</h1>
+  <h1>実はこのスライドもSvelte製</h1>
+  <div>
+    <img src="./qr.svg" alt="QR Code">
+  </div>
 </CenteringPane>
 
 <style>
   h1 {
     margin: 0;
+  }
+  img {
+    height: calc(var(--slide-height) / 2);
   }
 </style>
